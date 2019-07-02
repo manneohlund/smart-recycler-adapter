a9e7ae4cbc47f44db8dcb44fd5e1fe3e6d578d7a
manneohlund <manne.ohlund@gmail.com>
2019-07-03 01:33:41 +0200

    [FIX] Copyright version v2.0.1

--- sample/src/main/java/com/example/smartrecycleradapter/DemoActivity.java
+++ sample/src/main/java/com/example/smartrecycleradapter/DemoActivity.java
@@ -82,7 +82,7 @@ public class DemoActivity extends AppCompatActivity {
         items.add(new SciFiMoviesModel("Sci-Fi"));
         items.add(new MovieBannerModel("Promotion", MovieDataItems.INSTANCE.getRandomBanner()));
         items.add(new RecentlyPlayedMoviesModel("Recently played"));
-        items.add(new CopyrightModel("SmartRecyclerAdapter v2.0.0\n\nDeveloped by Manne Öhlund"));
+        items.add(new CopyrightModel("SmartRecyclerAdapter v2.0.1\n\nDeveloped by Manne Öhlund"));
 
         mainSmartMovieAdapter = SmartRecyclerAdapter
                 .items(items)
