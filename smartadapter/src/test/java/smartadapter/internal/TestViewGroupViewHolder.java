6e4b7ae2eecd2999b5463d6e0183ea872d02ed12
manneohlund <manne.ohlund@gmail.com>
2019-07-21 22:51:11 +0200

    [ADD] Test view holders for ReflectionsUtils tests

---
+++ smartadapter/src/test/java/smartadapter/internal/TestViewGroupViewHolder.java
@@ -0,0 +1,22 @@
+package smartadapter.internal;
+
+/*
+ * Created by Manne Öhlund on 2019-07-16.
+ * Copyright (c) All rights reserved.
+ */
+
+import android.view.ViewGroup;
+
+import smartadapter.viewholder.SmartViewHolder;
+
+public class TestViewGroupViewHolder extends SmartViewHolder {
+
+    public TestViewGroupViewHolder(ViewGroup view) {
+        super(view);
+    }
+
+    @Override
+    public void bind(Object item) {
+
+    }
+}
---
+++ smartadapter/src/test/java/smartadapter/internal/TestViewHolder.java
@@ -0,0 +1,22 @@
+package smartadapter.internal;
+
+/*
+ * Created by Manne Öhlund on 2019-07-16.
+ * Copyright (c) All rights reserved.
+ */
+
+import android.view.View;
+
+import smartadapter.viewholder.SmartViewHolder;
+
+public class TestViewHolder extends SmartViewHolder {
+
+    public TestViewHolder(View view) {
+        super(view);
+    }
+
+    @Override
+    public void bind(Object item) {
+
+    }
+}
