dcd1c3b443ef005dfca0c58caa48c90c23c76d56
manneohlund <manne.ohlund@gmail.com>
2019-08-06 00:52:57 +0200

    [ADD] SmartViewEventListenerHolder interface for undefined view event callback handling

---
+++ smartadapter/src/main/java/smartadapter/viewholder/SmartViewEventListenerHolder.java
@@ -0,0 +1,27 @@
+package smartadapter.viewholder;
+
+/*
+ * Created by Manne Öhlund on 2019-08-05.
+ * Copyright (c) All rights reserved.
+ */
+
+import android.support.annotation.NonNull;
+
+import smartadapter.listener.OnViewEventListener;
+
+/**
+ * Lets a view holder handle events with custom event ids.
+ * Implement this interface in your {@link SmartViewHolder} extension.
+ */
+public interface SmartViewEventListenerHolder {
+
+    /**
+     * Will be called from {@link smartadapter.SmartRecyclerAdapter} and
+     * will set {@link OnViewEventListener} if a default global listener is added to the adapter or
+     * target view holder is defined by {@link OnViewEventListener#getViewHolderType()}.
+     *
+     * @see OnViewEventListener
+     * @param viewEventListener listener for view events
+     */
+    void setOnViewEventListener(@NonNull OnViewEventListener viewEventListener);
+}
