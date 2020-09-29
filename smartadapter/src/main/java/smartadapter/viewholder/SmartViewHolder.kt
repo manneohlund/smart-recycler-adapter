404d074864ed4a26bb842db22805fc3a1308aa81
manneohlund <manne.ohlund@gmail.com>
2020-09-29 13:52:56 +0200

    Add default helper constructor with LayoutInflater to SmartViewHolder

--- smartadapter/src/main/java/smartadapter/viewholder/SmartViewHolder.kt
+++ smartadapter/src/main/java/smartadapter/viewholder/SmartViewHolder.kt
@@ -5,7 +5,10 @@ package smartadapter.viewholder
  * Copyright (c) All rights reserved.
  */
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
 
 import androidx.recyclerview.widget.RecyclerView
 
@@ -13,4 +16,8 @@ import androidx.recyclerview.widget.RecyclerView
  * Extension of [RecyclerView.ViewHolder] containing data item binding method.
  * @param <T> Data item type
  */
-abstract class SmartViewHolder<T : Any>(view: View) : RecyclerView.ViewHolder(view), BindableViewHolder<T>
+abstract class SmartViewHolder<T : Any>(view: View) : RecyclerView.ViewHolder(view), BindableViewHolder<T> {
+    constructor(parentView: ViewGroup, @LayoutRes layout: Int) : this(
+        LayoutInflater.from(parentView.context).inflate(layout, parentView, false)
+    )
+}
